fca9b8d220d80e42b681d83bc15f12192531c366
Kelly Guimarans <kelly.guimarans@gmail.com>
2023-02-17 19:42:29 -0300

    feat: desenvolvimento do componente ListaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,19 @@
 import CampoTexto from '../CompoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ];
+    
     return (
         <section className="formulario">
             <form>
@@ -10,6 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" items={times} />
             </form>
         </section>
     )
